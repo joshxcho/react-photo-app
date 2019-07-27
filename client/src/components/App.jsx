8defb0baf6cfdb11d08693586bcc534a03779f67
joshxcho <joshxcho123@gmail.com>
2019-07-27 14:54:37 -0700

    Get data from server to client

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,30 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+import Photos from './Photos';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      photos: [],
+    };
+
+    this.getAllPhotos = this.getAllPhotos.bind(this);
+  }
+
+  componentDidMount() {
+    this.getAllPhotos();
+  }
+
+  getAllPhotos() {
+    axios
+      .get('/api')
+      .then(res => this.setState({ photos: res.data }))
+      .catch(err => console.error(err));
+  }
+
   render() {
     return (
       <div>
