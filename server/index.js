e27f0745b4b3870efb145d6cf38c124b0892d4b5
joshxcho <joshxcho123@gmail.com>
2019-07-27 14:45:56 -0700

    Create api and fetch photos in the server

--- server/index.js
+++ server/index.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+const request = require('request');
+
 const express = require('express');
 
 const config = require('../config');
@@ -8,5 +11,19 @@ app.set('port', config.port);
 
 app.use(express.static('public/'));
 app.use(express.static('client/dist'));
+
+app.get('/api', (req, res) => {
+  const url = process.env.URL;
+  const photoEndpoint = 'photos/?client_id=';
+  const apiKey = process.env.API_ACCESS_KEY;
+  const fullUrl = `${url}${photoEndpoint}${apiKey}`;
+
+  request(fullUrl, (error, response, body) => {
+    if (error) {
+      res.status(400).send(error);
+    }
+    res.status(200).send(body);
+  });
+});
 
 app.listen(app.get('port'), () => console.log(`Listening at ${app.get('port')}!`));
